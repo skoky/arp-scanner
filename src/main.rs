0b68c81aecb4828c10d9a546a167859614c6f113
Skoky <(skokys@gmail.com)>
2020-12-18 12:29:49 +0100

    removing banner, de-duplicating, sorting, remove csv title

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,27 @@
-
-extern crate pnet;
-extern crate ipnetwork;
 extern crate clap;
-
+extern crate ipnetwork;
+extern crate pnet;
 #[macro_use]
 extern crate prettytable;
 
-use clap::{Arg, App, ArgMatches};
-
-use prettytable::Table;
-use prettytable::format;
-
-use std::time::Duration;
-use std::thread;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::Write;
-use std::sync::mpsc::{self, Sender, Receiver};
 use std::net::{IpAddr, Ipv4Addr};
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::thread;
+use std::time::Duration;
 
-use ipnetwork::{IpNetwork};
-use pnet::datalink::{self, Channel, NetworkInterface, MacAddr};
-
-use pnet::packet::ethernet::MutableEthernetPacket;
+use clap::{App, Arg, ArgMatches};
+use ipnetwork::IpNetwork;
+use pnet::datalink::{self, Channel, MacAddr, NetworkInterface};
+use pnet::packet::{MutablePacket, Packet};
+use pnet::packet::arp::{ArpHardwareTypes, ArpOperation, ArpOperations, ArpPacket};
 use pnet::packet::arp::MutableArpPacket;
-use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
-use pnet::packet::{Packet, MutablePacket};
-use pnet::packet::arp::{ArpHardwareTypes, ArpOperations, ArpOperation, ArpPacket};
-
-const BANNER : &str = r#"
-  ____  ____   ____        _____   __   ____  ____   ____     ___  ____  
- /    ||    \ |    \      / ___/  /  ] /    ||    \ |    \   /  _]|    \ 
-|  o  ||  D  )|  o  )    (   \_  /  / |  o  ||  _  ||  _  | /  [_ |  D  )
-|     ||    / |   _/      \__  |/  /  |     ||  |  ||  |  ||    _]|    / 
-|  _  ||    \ |  |        /  \ /   \_ |  _  ||  |  ||  |  ||   [_ |    \ 
-|  |  ||  .  \|  |        \    \     ||  |  ||  |  ||  |  ||     ||  .  \
-|__|__||__|\_||__|         \___|\____||__|__||__|__||__|__||_____||__|\_|
-
-"#;
+use pnet::packet::ethernet::{EthernetPacket, EtherTypes};
+use pnet::packet::ethernet::MutableEthernetPacket;
+use prettytable::format;
+use prettytable::Table;
 
 fn send_arp_packet(
     interface: NetworkInterface,
@@ -45,7 +30,6 @@ fn send_arp_packet(
     target_ip: Ipv4Addr,
     target_mac: MacAddr,
     arp_operation: ArpOperation) {
-
     let (mut tx, _) = match datalink::channel(&interface, Default::default()) {
         Ok(Channel::Ethernet(tx, rx)) => (tx, rx),
         Ok(_) => panic!("Unknown channel type"),
@@ -78,7 +62,6 @@ fn send_arp_packet(
 }
 
 fn recv_arp_packets(interface: NetworkInterface, tx: Sender<(Ipv4Addr, MacAddr)>) {
-
     thread::spawn(move || {
         let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
             Ok(Channel::Ethernet(tx, rx)) => (tx, rx),
@@ -98,7 +81,7 @@ fn recv_arp_packets(interface: NetworkInterface, tx: Sender<(Ipv4Addr, MacAddr)>
                         let result: (Ipv4Addr, MacAddr) = (arp_packet.get_sender_proto_addr(), arp_packet.get_sender_hw_addr());
                         tx.send(result).unwrap();
                     }
-                },
+                }
                 Err(e) => panic!("An error occurred while reading packet: {}", e)
             }
         }
@@ -106,61 +89,58 @@ fn recv_arp_packets(interface: NetworkInterface, tx: Sender<(Ipv4Addr, MacAddr)>
 }
 
 fn main() {
-
-	let matches = App::new(BANNER)
-		.author("Gavyn Riebau <gavyn.riebau@gmail.com>")
-		.about("\nRuns an ARP scan to discover all hosts in the network")
-		.arg(Arg::with_name("interface")
-			.short("i")
-			.long("interface")
-			.value_name("INTERFACE")
-			.help("The interface on which the scan will be performed")
-			.required_unless_one(&["list", "interface_index"])
-			.conflicts_with("interface_index")
-		)
-		.arg(Arg::with_name("interface_index")
-			.short("x")
-			.long("index")
-			.value_name("INTERFACE_INDEX")
-			.help("The index of the interface rather than the interface name.")
-			.required_unless_one(&["list", "interface"])
-			.conflicts_with("interface")
-		)
-		.arg(Arg::with_name("list")
-			.short("l")
-			.long("list")
-			.help("List available interfaces including their index")
-			.conflicts_with("interface")
-		)
+    let matches = App::new("arp-scanner")
+        .author("Gavyn Riebau <gavyn.riebau@gmail.com>, extended by skokys@gmail.com")
+        .about("\nRuns an ARP scan to discover all hosts in the network")
+        .arg(Arg::with_name("interface")
+            .short("i")
+            .long("interface")
+            .value_name("INTERFACE")
+            .help("The interface on which the scan will be performed")
+            .required_unless_one(&["list", "interface_index"])
+            .conflicts_with("interface_index")
+        )
+        .arg(Arg::with_name("interface_index")
+            .short("x")
+            .long("index")
+            .value_name("INTERFACE_INDEX")
+            .help("The index of the interface rather than the interface name.")
+            .required_unless_one(&["list", "interface"])
+            .conflicts_with("interface")
+        )
+        .arg(Arg::with_name("list")
+            .short("l")
+            .long("list")
+            .help("List available interfaces including their index")
+            .conflicts_with("interface")
+        )
         .arg(Arg::with_name("output")
-             .short("o")
-             .long("out")
-             .value_name("FILE")
-             .help("Write results to a file in CSV format")
-         )
-		.get_matches();
+            .short("o")
+            .long("out")
+            .value_name("FILE")
+            .help("Write results to a file in CSV format")
+        )
+        .get_matches();
 
-	println!("{}", BANNER);
-
-	if matches.is_present("list") {
-		println!("Listing interfaces:\n");
-		let interfaces = datalink::interfaces();
-		for interface in interfaces.iter() {
-			println!("{}\n", interface);
-		}
-		std::process::exit(0);
-	}
+    if matches.is_present("list") {
+        println!("Listing interfaces:\n");
+        let interfaces = datalink::interfaces();
+        for interface in interfaces.iter() {
+            println!("{}\n", interface);
+        }
+        std::process::exit(0);
+    }
 
     let interface_match = |iface: &NetworkInterface| {
-		if matches.is_present("interface") {
-			return &iface.name == matches.value_of("interface").unwrap();
-		} else {
-			let index_str = matches.value_of("interface_index").unwrap();
-			let index : u32 = index_str.parse().unwrap();
+        if matches.is_present("interface") {
+            return &iface.name == matches.value_of("interface").unwrap();
+        } else {
+            let index_str = matches.value_of("interface_index").unwrap();
+            let index: u32 = index_str.parse().unwrap();
 
-			return iface.index == index;
-		}
-	};
+            return iface.index == index;
+        }
+    };
 
     let interfaces = datalink::interfaces();
     let interface = interfaces.into_iter()
@@ -168,15 +148,15 @@ fn main() {
         .next()
         .unwrap();
 
-	if interface.is_loopback() {
-		println!("Aborting because chosen interface is a loopback interface.\nChoose a non-loopback interface.\n");
-		std::process::exit(1);
-	}
+    if interface.is_loopback() {
+        println!("Aborting because chosen interface is a loopback interface.\nChoose a non-loopback interface.\n");
+        std::process::exit(1);
+    }
 
-	if interface.ips.is_empty() {
-		println!("Aborting because chosen interface doesn't have a network address.\n");
-		std::process::exit(1);
-	}
+    if interface.ips.is_empty() {
+        println!("Aborting because chosen interface doesn't have a network address.\n");
+        std::process::exit(1);
+    }
 
     println!("Using interface: {}\n", interface);
 
@@ -184,7 +164,7 @@ fn main() {
     let source_network = interface.ips.iter().find(|x| x.is_ipv4()).unwrap();
     let source_ip = source_network.ip();
     let arp_operation = ArpOperations::Request;
-    let target_mac = MacAddr::new(255,255,255,255,255,255);
+    let target_mac = MacAddr::new(255, 255, 255, 255, 255, 255);
 
     // Channel for ARP replies.
     let (tx, rx): (Sender<(Ipv4Addr, MacAddr)>, Receiver<(Ipv4Addr, MacAddr)>) = mpsc::channel();
@@ -199,48 +179,58 @@ fn main() {
                 match source_ip {
                     IpAddr::V4(source_ipv4) => {
                         send_arp_packet(interface.clone(), source_ipv4, source_mac, target_ipv4, target_mac, arp_operation);
-                    },
+                    }
                     e => panic!("Error while parsing to IPv4 address: {}", e)
                 }
-
             }
-        },
+        }
         e => panic!("Error while attempting to get network for interface: {}", e)
     }
     println!("[X] Collecting results...");
     thread::sleep(Duration::from_secs(2));
     println!();
 
-    let mut table = Table::new();
-    table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
-    table.set_titles(row!["host", "mac"]);
+    let mut macs: HashSet<(Ipv4Addr, MacAddr)> = HashSet::new();
 
     loop {
         match rx.try_recv() {
             Ok((ipv4_addr, mac_addr)) => {
-                table.add_row(row![ipv4_addr, mac_addr]);
-            },
+                let _ = macs.insert((ipv4_addr, mac_addr));
+            }
             Err(_) => break
         }
     }
 
-	if table.len() > 0 {
-		table.printstd();
+    let mut x: Vec<(Ipv4Addr, MacAddr)> = macs.into_iter().collect::<Vec<_>>();
+    x.sort_by(|x,y| {
+        x.0.cmp(&y.0)
+    });
+
+    let v = x.into_iter()
+        .map(|x| {
+        row![x.0,x.1]
+    }).collect::<Vec<_>>();
+
+    let mut table = Table::init(v);
+    table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    table.set_titles(row!["host", "mac"]);
+
+    if table.len() > 0 {
+        table.printstd();
 
         if matches.is_present("output") {
             let out_file = get_out_file(&matches);
+            table.unset_titles();
             let _ = table.to_csv(out_file).expect("Failed to write results to CSV.");
         }
-
-	} else {
-		println!("No hosts found...");
+    } else {
+        println!("No hosts found...");
 
         if matches.is_present("output") {
             let mut out_file = get_out_file(&matches);
             let _ = out_file.write_all(b"No hosts found...").unwrap();
         }
-	}
-
+    }
 }
 
 fn get_out_file(matches: &ArgMatches) -> File {
